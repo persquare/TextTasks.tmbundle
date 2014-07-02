a65586d6fa2744df253e4c3eebb8ce4615a83d52
Per Persson <>
2014-07-02 08:03:12 +0200

    Update

--- Support/tt_config.py
+++ Support/tt_config.py
@@ -18,4 +18,35 @@ PROJECT_DIRS = ['${HOME}']
 #
 FILE_EXTS = ['.todo', '.tasks']
 
+##
+# The dedicated inbox file
+#
+INBOX = '${HOME}/inbox.todo'
+
+##
+# The tags that populate the tag pop-up list
+#
+TAGS = ['next', 'remind', 'due']
+
+##
+# Quic-list tags and any tag with a date at most n days ahead 
+#
+QUICK_LIST_TAGS = ['next']
+QUICK_LIST_DAYS_AHEAD = 1
+
+##
+# Special sections
+#
+ARCHIVE_SECTION = Archive
+REFERENCE_SECTION = References
+ISSUE_SECTION = Bugs
+
+##
+# Enable/disable TextTasks helpers
+#
+ENABLE_INBOX_CLI = False
+ENABLE_INBOX_APP = False
+ENABLE_MAIL_APP_RULES = False
+
+
 
